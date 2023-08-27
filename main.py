6320d51c3e6daa80242c66a86a2928bb173f196a
Sung Joon Park <park_sungjoon@hotmail.com>
2023-08-28 00:01:24 +0900

    refactor: remove unnecessary comments

--- main.py
+++ main.py
@@ -28,32 +28,24 @@ def main():
     # h_data = fetch_historical_data(combined_list, date_start, date_today)
     h_data = fetch_saved_data()
 
-    # Calculate return and risk data for each ticker
     return_risk_data, return_df = calculate_risk_return(h_data)
 
-    # Get user input for desired tickers and minimum weights
     valid_tickers, min_weights = get_user_input(return_risk_data)
 
-    # Prepare portfolio data for optimization
     return_data, risk_data, cov_matrix_input = prepare_portfolio_data(
         return_risk_data, valid_tickers, return_df)
 
-    # Calculate possible return range based on user-inputted minimum weights
     min_return, max_return = calculate_return_range(
         min_weights, return_risk_data, valid_tickers, return_data)
 
-    # Get user's desired expected return within the calculated range
     user_return = get_user_desired_return(min_return, max_return)
 
-    # Optimize portfolio based on user's input and calculated data
     optimal_portfolio, portfolio_return, portfolio_std_dev = optimize(
         min_weights, valid_tickers, cov_matrix_input, return_data, user_return)
 
-    # Display portfolio optimization results 
     display_output(valid_tickers, optimal_portfolio,
                    portfolio_return, portfolio_std_dev)
     
-    # Plot portfolio optimization results 
     plot_portfolio(valid_tickers, optimal_portfolio,
                    portfolio_return, portfolio_std_dev)
 
